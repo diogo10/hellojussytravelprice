ae45bdda8742bef333b1695b63f5796e7e9a6977
Diogo Ribeiro <diogjp10@gmail.com>
2022-02-11 08:30:23 +0000

    add 5 max cost for now

--- util.js
+++ util.js
@@ -14,9 +14,9 @@ function calculateValueFor(obj) {
     }else if(value > 19000 && value <= 30000) {
       result = 5;
     }else if(value > 30000 && value <= 42000) {
-      result = 6;
+      result = 5;
     }else {
-      result = 8;
+      result = 5;
     }
   
     return result; 
